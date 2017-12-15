d35a4c80ebd6b3479b0ae38459f98b9204689398
Kia Choi <sgchoi529@email.arizona.edu>
2017-12-15 12:42:17 -0700

    Modified user.js & routes.js - TODO: refine

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,17 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   // there should be a user object set on req
   // use that req.user object to create a user and save it to our Mongo instance.
+  const { username, password } = req.user;
+  const newUser = new User({ username, password });
+  newUser.save((err, savedUser) =>{
+    if (err) {
+      res.status(422);
+      res.json({ 'Cannot create User': err });
+      return;
+    }
+    res.json(savedUser);
+    next();
+  });
 };
 
 module.exports = {
