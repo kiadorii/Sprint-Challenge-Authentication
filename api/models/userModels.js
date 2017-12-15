3b088a7aabdef36a7994b6e1bbb13837011fcd5a
Kia Choi <sgchoi529@email.arizona.edu>
2017-12-15 14:16:09 -0700

    Deleted extra from comment - userModels.js

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
 mongoose.models = {};
-mongoose.modelSchemas = {};
-
-mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new Schema({
   username: {
